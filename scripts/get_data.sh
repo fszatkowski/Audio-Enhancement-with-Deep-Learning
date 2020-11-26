642cc14836518eef266d0f3974f948792287db37
filip <fszatkowski96@gmail.com>
2020-11-26 16:13:03 +0100

    Fix data download script

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -2,8 +2,10 @@
 
 root=$(git rev-parse --show-toplevel)
 cd ${root}
-output_dir="data/fma"
+output_dir="data/fma/"
 
 mkdir -p ${output_dir}
-curl https://os.unil.cloud.switch.ch/fma/fma_small.zip -o ${output_dir}
-unzip ${output_dir}/fma_small.zip
+
+zip_file="${output_dir}/fma_small.zip"
+curl https://os.unil.cloud.switch.ch/fma/fma_small.zip -o ${zip_file}
+unzip ${zip_file}
