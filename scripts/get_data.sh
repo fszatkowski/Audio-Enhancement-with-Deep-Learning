922fb0c5a644065cf00aabd2fbb88e293784a32e
filip <fszatkowski96@gmail.com>
2020-11-28 12:59:56 +0100

    Add output_dir to unzip in scripts/get_data.sh

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -8,4 +8,4 @@ mkdir -p ${output_dir}
 
 zip_file="${output_dir}/fma_small.zip"
 curl https://os.unil.cloud.switch.ch/fma/fma_small.zip -o ${zip_file}
-unzip ${zip_file}
+unzip ${zip_file} -d "${output_dir}"
