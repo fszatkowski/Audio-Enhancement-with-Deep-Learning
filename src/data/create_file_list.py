5809f8532423cae990c261f613259756ad8f95e6
filip <fszatkowski96@gmail.com>
2020-11-28 15:56:04 +0100

    Slight edit to script creating file list

--- src/data/create_file_list.py
+++ src/data/create_file_list.py
@@ -4,19 +4,20 @@ from typing import Sequence
 
 from tqdm import tqdm
 
+from constants import RAW_DATASET_FILELIST
 from data.utils import get_mp3_files, load_file
 
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
-    parser.add_argument("--output_file", "-o", type=str, required=True)
+    parser.add_argument("--output_file", "-o", type=str, default=RAW_DATASET_FILELIST)
     parser.add_argument("--n_files", "-n", type=int, default=0)
     return parser.parse_args()
 
 
 def create_file_list(files: Sequence[str], n_files: int, output_file_name: str):
     """ Create text file with names of relevant audio files
-    (relevant meaning 2 channels, 44100 original sampling rate and no errors during loading
+    (meaning those files have 2 channels, 44100 original sampling rate and produce no errors during loading
     Those files are used for training base models """
     if os.path.exists(output_file_name):
         os.remove(output_file_name)
