df22c8ddd436e576dc2098c9ad4fe6ba58a65e44
filip <fszatkowski96@gmail.com>
2020-12-01 10:34:22 +0100

    Add test script

--- scripts/train/autoencoder/test.sh
+++ scripts/train/autoencoder/test.sh
@@ -8,13 +8,13 @@ SEEDS=${1-"111 1111 11111"}
 
 for seed in ${SEEDS}; do
     python3 src/autoencoder/train.py \
-      --epochs 20 \
-      --warmup_epochs 10 \
-      --patience 20 \
+      --epochs 2 \
+      --warmup_epochs 2 \
+      --patience 4 \
       --batch_size 64 \
-      --train_files 512 \
-      --test_files 128 \
-      --val_files 128 \
+      --train_files 64 \
+      --test_files 64 \
+      --val_files 64 \
       --save_every_n_steps 100 \
       --transformations "default" \
       --max_transformations_applied 2 \
@@ -25,5 +25,5 @@ for seed in ${SEEDS}; do
       --random_seed ${seed} \
       --activation "prelu" \
       --norm batch_norm \
-      --model_dir "models/autoencoder/new_${seed}"
+      --model_dir "models/autoencoder/test_${seed}"
 done
